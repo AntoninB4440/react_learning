60d6061ea0e3566af2a75b7eada27a79bb0a488e
Antonin_B <barceloantonin@gmail.com>
2025-09-20 21:19:01 +0200

    Style et organisation

---
+++ react_fact/src/MainContent.jsx
@@ -0,0 +1,12 @@
+export default function MainContent(){
+    return (
+        <main>
+            <h1>Raisons de pourquoi j'ai envie d'apprendre React</h1>
+            <ol>
+                <li>Apprendre un nouveau Framework</li>
+                <li>Ajouter une nouvelle ligne à mes compétences</li>
+                <li>Être plus employable</li>
+            </ol>
+        </main>
+    )
+}
--- react_fact/src/index.jsx
+++ react_fact/src/index.jsx
@@ -1,4 +1,8 @@
 import { createRoot } from 'react-dom/client';
+//import { Fragment } from 'react'; -- Evite d'insérer une <div> pour entourer le code et donc d'ajouter un nouveau noeud html, possibilité d'utiliser <></> pour ne pas importer Fragment (fait la même chose)
+import Header from './Header';
+import MainContent from './MainContent';
+import Footer from './Footer';
 
 const root = createRoot(document.getElementById('root'));
 root.render(
@@ -12,5 +16,17 @@ root.render(
             <li>Is maintained by Facebook</li>
             <li>Powers thousands of enterprise apps, including mobile apps</li>
         </ul>
+        <Page />
     </main>
+    
 )
+
+function Page() {
+    return (
+        <>
+            <Header />
+            <MainContent />
+            <Footer />
+        </>
+    )
+}

5ffff1426a32b7ce9e37fe4fc74ff3315ea9aba9
Antonin_B <barceloantonin@gmail.com>
2025-09-21 18:36:28 +0200

    First project done

---
+++ react_fact/src/project/App.jsx
@@ -0,0 +1,11 @@
+import Navbar from "./component/Navbar";
+import Main from "./component/Main"
+
+export default function App(){
+    return(
+        <>
+            <Navbar />
+            <Main />
+        </>
+    )
+}
---
+++ react_fact/src/project/component/Navbar.jsx
@@ -0,0 +1,11 @@
+export default function Navbar(){
+    return (
+        <header className="header">
+            <nav className="navbar">
+                <img className="navBarLogo" src="./src/assets/react.svg" alt="React Logo"/>
+                <span className="navTitle">ReactFacts</span>
+            </nav>
+        </header>
+        
+    )
+}

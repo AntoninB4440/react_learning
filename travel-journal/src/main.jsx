5a3c1f1105bf8ad0ec06d10128c2a727757294c3
Antonin_B <barceloantonin@gmail.com>
2025-09-24 19:26:22 +0200

    Travel-journal : Mapping all Entry

--- travel-journal/src/main.jsx
+++ travel-journal/src/main.jsx
@@ -1,13 +1,27 @@
 import { createRoot } from "react-dom/client"
 import Header from "./component/Header";
 import Entry from "./component/Entry";
+import data from "./data.js"
 
 const root = createRoot(document.getElementById("root"));
+
+const travels = data.map((travel) => {
+    return <Entry
+            key={travel.id}
+            img={travel.img}
+            title={travel.title}
+            country={travel.country}
+            googleMapsLink={travel.googleMapsLink}
+            dates={travel.dates}
+            description={travel.text}
+        />
+})
+
 root.render(
     <>
         <Header />
         <div className="container">
-            <Entry />
+            {travels}
         </div>
     </>
 )

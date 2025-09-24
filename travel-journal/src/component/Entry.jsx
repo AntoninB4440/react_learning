1d52db624e25cabe33bc65017ac60e31855cd5c7
Antonin_B <barceloantonin@gmail.com>
2025-09-24 19:26:22 +0200

    Travel-journal : Mapping all Entry

--- travel-journal/src/component/Entry.jsx
+++ travel-journal/src/component/Entry.jsx
@@ -1,16 +1,19 @@
-export default function Entry(){
+import markerUrl from '../assets/marker.png'
+
+export default function Entry(props){
+    console.log(props)
     return (
         <article className="entry-container">
             <div className="entry-image-container">
-                <img className="entry-image" src="https://scrimba.com/links/travel-journal-japan-image-url" alt="Mount Fuji" />
+                <img className="entry-image" src={props.entry.img.src} alt={props.entry.img.alt} />
             </div>
             <div className="entry-info-container">
-                <img src="./src/assets/marker.png" alt="Map marker icon" className="entry-marker"/>
-                <span className="entry-country">Japan</span>
-                <a target="_blank" href="https://www.google.com/maps/place/Mount+Fuji/@35.3606421,138.7170637,15z/data=!3m1!4b1!4m6!3m5!1s0x6019629a42fdc899:0xa6a1fcc916f3a4df!8m2!3d35.3606255!4d138.7273634!16zL20vMGNrczA?entry=ttu">View on Google Maps</a>
-                <h2 className="entry-title">Mont Fuji</h2>
-                <p className="entry-dates">12 Jan, 2021 - 24 Jan, 2021</p>
-                <p className="entry-description">Mount Fuji is the tallest mountain in Japan, standing at 3,776 meters (12,380 feet). Mount Fuji is the single most popular tourist site in Japan, for both Japanese and foreign tourists.</p>
+                <img src={markerUrl} alt="Map marker icon" className="entry-marker"/>
+                <span className="entry-country">{props.entry.country}</span>
+                <a target="_blank" href={props.entry.googleMapsLink}>View on Google Maps</a>
+                <h2 className="entry-title">{props.entry.title}</h2>
+                <p className="entry-dates">{props.entry.dates}</p>
+                <p className="entry-description">{props.entrydescription}</p>
             </div>
         </article>
     )
--- travel-journal/src/main.jsx
+++ travel-journal/src/main.jsx
@@ -1,13 +1,22 @@
 import { createRoot } from "react-dom/client"
 import Header from "./component/Header";
 import Entry from "./component/Entry";
+import data from "./data.js"
 
 const root = createRoot(document.getElementById("root"));
+
+const travels = data.map((travel) => {
+    return <Entry
+            key={travel.id}
+            enrty={travel}
+        />
+})
+
 root.render(
     <>
         <Header />
         <div className="container">
-            <Entry />
+            {travels}
         </div>
     </>
 )
